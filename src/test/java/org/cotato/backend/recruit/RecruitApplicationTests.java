961440af92d4c2076c734a372fb14f019b88972f
jeongchanmin <jjw421412@naver.com>
2025-12-17 16:27:09 +0900

    feat: 인증 관련 에러코드 추가

--- src/test/java/org/cotato/backend/recruit/RecruitApplicationTests.java
+++ src/test/java/org/cotato/backend/recruit/RecruitApplicationTests.java
@@ -1,22 +1,10 @@
 package org.cotato.backend.recruit;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 @SpringBootTest
-
-	@TestConfiguration
-	static class TestConfig {
-		@Bean
-		@Primary
-		public ObjectMapper objectMapper() {
-			return new ObjectMapper();
-		}
-	}
+class RecruitApplicationTests {
 
 	@Test
 	void contextLoads() {}
