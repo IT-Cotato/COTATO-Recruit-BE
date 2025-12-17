ab6223d495f4bd49dc1ecd06e8b9765e4ecb3ea4
jeongchanmin <jjw421412@naver.com>
2025-12-17 14:55:06 +0900

    chore: Docker Compose에 Redis 컨테이너 추가
    - Redis 7-alpine 이미지 추가
    - blue/green 컨테이너에 Redis 연결 설정 추가
    - OAuth2 환경변수 추가
    - redis_data 볼륨 추가

--- src/test/java/org/cotato/backend/recruit/RecruitApplicationTests.java
+++ src/test/java/org/cotato/backend/recruit/RecruitApplicationTests.java
@@ -1,10 +1,22 @@
 package org.cotato.backend.recruit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootTest
-class RecruitApplicationTests {
+
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		@Primary
+		public ObjectMapper objectMapper() {
+			return new ObjectMapper();
+		}
+	}
 
 	@Test
 	void contextLoads() {}
