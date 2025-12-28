c3544df230a89dd8b8bac2b7b10ef6a2f7bd1b9c
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-12-28 17:16:27 +0900

    feat : Generation 엔티티, 리포지토리 생성

--- src/main/java/org/cotato/backend/recruit/domain/generation/entity/Generation.java
+++ src/main/java/org/cotato/backend/recruit/domain/generation/entity/Generation.java
@@ -2,8 +2,6 @@ package org.cotato.backend.recruit.domain.generation.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -25,7 +23,12 @@ public class Generation {
 	private boolean isRecruitingActive;
 
 	@Builder
-	public Generation(boolean isRecruitingActive) {
+	public Generation(Long id, boolean isRecruitingActive) {
+		this.id = id;
+		this.isRecruitingActive = isRecruitingActive;
+	}
+
+	public void updateRecruitmentStatus(boolean isRecruitingActive) {
 		this.isRecruitingActive = isRecruitingActive;
 	}
 }
---
+++ src/main/java/org/cotato/backend/recruit/domain/generation/repository/GenerationRepository.java
@@ -0,0 +1,9 @@
+package org.cotato.backend.recruit.domain.generation.repository;
+
+import java.util.Optional;
+import org.cotato.backend.recruit.domain.generation.entity.Generation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenerationRepository extends JpaRepository<Generation, Long> {
+	Optional<Generation> findById(Long id);
+}
