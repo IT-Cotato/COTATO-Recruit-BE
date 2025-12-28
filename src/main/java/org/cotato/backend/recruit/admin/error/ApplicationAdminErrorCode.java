d03a647c1ed897cf5d1045e4244cda0809d9cad9
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-12-28 19:42:45 +0900

    feat : adminErrorCode 구현

--- src/main/java/org/cotato/backend/recruit/admin/error/ApplicationAdminErrorCode.java
+++ src/main/java/org/cotato/backend/recruit/admin/error/ApplicationAdminErrorCode.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ApplicationAdminErrorCode {
-    GENERATION_NOT_FOUND(HttpStatus.NOT_FOUND, "GEN-001", "해당 기수를 찾을 수 없습니다.");
+	GENERATION_NOT_FOUND(HttpStatus.NOT_FOUND, "GEN-001", "해당 기수를 찾을 수 없습니다.");
 
-    private final HttpStatus status;
-    private final String code;
-    private final String message;
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
 }
