b0a868d8bd1526ad206cacf6d97af994f848b1a0
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-12-31 12:09:36 +0900

    feat : 운영진평가 service 구현

---
+++ src/main/java/org/cotato/backend/recruit/admin/service/staffEvaluation/StaffEvaluationService.java
@@ -0,0 +1,75 @@
+package org.cotato.backend.recruit.admin.service.staffEvaluation;
+
+import lombok.RequiredArgsConstructor;
+import org.cotato.backend.recruit.admin.dto.request.staffEvaluation.CreateStaffEvaluationRequest;
+import org.cotato.backend.recruit.admin.dto.response.staffEvaluation.StaffEvaluationResponse;
+import org.cotato.backend.recruit.admin.service.applicationAdmin.ApplicationAdminService;
+import org.cotato.backend.recruit.domain.application.entity.Application;
+import org.cotato.backend.recruit.domain.evaluation.entity.Evaluation;
+import org.cotato.backend.recruit.domain.evaluation.enums.EvaluatorType;
+import org.cotato.backend.recruit.domain.evaluation.repository.EvaluationRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StaffEvaluationService {
+
+	private final EvaluationRepository evaluationRepository;
+	private final ApplicationAdminService applicationAdminService;
+
+	private void validate(EvaluatorType evaluatorType) {
+		if (evaluatorType == null) {
+			throw new IllegalArgumentException("EvaluatorType is required");
+		}
+	}
+
+	public StaffEvaluationResponse getEvaluation(Long applicationId, EvaluatorType evaluatorType) {
+		validate(evaluatorType);
+
+		Application application = applicationAdminService.getApplication(applicationId);
+		Evaluation evaluation =
+				evaluationRepository
+						.findByApplicationIdAndEvaluatorType(application.getId(), evaluatorType)
+						.orElse(null);
+
+		return StaffEvaluationResponse.from(evaluation != null ? evaluation.getComment() : null);
+	}
+
+	@Transactional
+	public void createEvaluation(Long applicationId, CreateStaffEvaluationRequest request) {
+		validate(request.evaluatorType());
+
+		if (request.comment() == null) {
+			throw new IllegalArgumentException("필수 값이 누락되었습니다.");
+		}
+
+		Application application = applicationAdminService.getApplication(applicationId);
+
+		Evaluation evaluation =
+				evaluationRepository
+						.findByApplicationIdAndEvaluatorType(
+								application.getId(), request.evaluatorType())
+						.orElse(null);
+
+		if (evaluation != null) {
+			update(evaluation, request);
+		} else {
+			insert(application, request);
+		}
+	}
+
+	private void update(Evaluation evaluation, CreateStaffEvaluationRequest request) {
+		evaluation.updateComment(request.comment());
+	}
+
+	private void insert(Application application, CreateStaffEvaluationRequest request) {
+		evaluationRepository.save(
+				Evaluation.builder()
+						.application(application)
+						.evaluatorType(request.evaluatorType())
+						.comment(request.comment())
+						.build());
+	}
+}
