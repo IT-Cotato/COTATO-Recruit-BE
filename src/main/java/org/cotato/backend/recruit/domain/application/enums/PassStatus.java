ef891e5c51bd4b1495228fe8d5d0c82579385aee
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-12-31 12:05:15 +0900

    feat : 상수 검증 메소드 구현

--- src/main/java/org/cotato/backend/recruit/domain/application/enums/PassStatus.java
+++ src/main/java/org/cotato/backend/recruit/domain/application/enums/PassStatus.java
@@ -12,4 +12,26 @@ public enum PassStatus {
 	PENDING("평가 전");
 
 	private final String description;
+
+	public static PassStatus fromString(String name) {
+		isValidPassStatus(name);
+		return valueOf(name);
+	}
+
+	public static void isValidPassStatus(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("PassStatus은 null일 수 없습니다.");
+		}
+
+		if (!name.equals(name.toUpperCase())) {
+			throw new IllegalArgumentException("PassStatus은 대문자로 입력해야 합니다.");
+		}
+
+		if (!name.equals("PASS")
+				&& !name.equals("FAIL")
+				&& !name.equals("WAITLISTED")
+				&& !name.equals("PENDING")) {
+			throw new IllegalArgumentException("유효하지 않은 합격 여부입니다.");
+		}
+	}
 }
--- src/main/java/org/cotato/backend/recruit/domain/question/enums/PartType.java
+++ src/main/java/org/cotato/backend/recruit/domain/question/enums/PartType.java
@@ -14,4 +14,30 @@ public enum PartType {
 	ETC("기타");
 
 	private final String description;
+
+	// from String
+	public static PartType fromString(String name) {
+		isValidPartType(name);
+		return valueOf(name);
+	}
+
+	// validate
+	public static void isValidPartType(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("PartType은 null일 수 없습니다.");
+		}
+
+		if (!name.equals(name.toUpperCase())) {
+			throw new IllegalArgumentException("PartType은 대문자로 입력해야 합니다.");
+		}
+
+		// BE,FE,DE,PM,ETC
+		if (!name.equals("BE")
+				&& !name.equals("FE")
+				&& !name.equals("DE")
+				&& !name.equals("PM")
+				&& !name.equals("ETC")) {
+			throw new IllegalArgumentException("유효하지 않은 파트 타입입니다.");
+		}
+	}
 }
