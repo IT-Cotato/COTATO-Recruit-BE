196f88b1a160b1f64c20f3b811c4e23d37313ba0
jeongchanmin <jjw421412@naver.com>
2026-01-01 22:06:05 +0900

    refactor: 사용하지 않는 EmailContentUpdateRequest 삭제

--- src/main/java/org/cotato/backend/recruit/admin/dto/request/email/EmailContentUpdateRequest.java
+++
@@ -1,5 +0,0 @@
-package org.cotato.backend.recruit.admin.dto.request.email;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record EmailContentUpdateRequest(@NotBlank(message = "메일 내용은 필수입니다.") String content) {}
