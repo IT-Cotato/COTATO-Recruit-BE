fc935691c7a2216267c298ce3a6f6464c96d9d2f
jeongchanmin <jjw421412@naver.com>
2026-01-01 13:06:22 +0900

    refactor: 패키지 변경

--- src/main/java/org/cotato/backend/recruit/domain/recruitment/Repository/RecruitmentInformationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cotato.backend.recruit.domain.recruitment.Repository;
-
-import org.cotato.backend.recruit.domain.recruitment.entity.RecruitmentInformation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecruitmentInformationRepository
-		extends JpaRepository<RecruitmentInformation, Long> {}
