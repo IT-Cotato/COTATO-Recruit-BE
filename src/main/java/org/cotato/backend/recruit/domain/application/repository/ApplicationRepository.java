31f426c91631cd7e26cc0a3cd6b4821c226f3b42
chanmin <66260209+chanmin-00@users.noreply.github.com>
2026-01-02 11:29:26 +0900

    Revert "feat : 메일 조회/수정/전송 API 구현 (#48)"
    This reverts commit 8d5c7847bdc40bc46574303f396873ebb5964b97.

--- src/main/java/org/cotato/backend/recruit/domain/application/repository/ApplicationRepository.java
+++ src/main/java/org/cotato/backend/recruit/domain/application/repository/ApplicationRepository.java
@@ -1,9 +1,7 @@
 package org.cotato.backend.recruit.domain.application.repository;
 
-import java.util.List;
 import java.util.Optional;
 import org.cotato.backend.recruit.domain.application.entity.Application;
-import org.cotato.backend.recruit.domain.application.enums.PassStatus;
 import org.cotato.backend.recruit.domain.generation.entity.Generation;
 import org.cotato.backend.recruit.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,22 +18,4 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 	 * @return 지원서 (Optional)
 	 */
 	Optional<Application> findByUserAndGeneration(User user, Generation generation);
-
-	/**
-	 * 특정 기수의 특정 합격 상태를 가진 지원서 목록 조회
-	 *
-	 * @param generation 기수
-	 * @param passStatus 합격 상태
-	 * @return 지원서 목록
-	 */
-	List<Application> findByGenerationAndPassStatus(Generation generation, PassStatus passStatus);
-
-	/**
-	 * 특정 기수의 특정 합격 상태를 가진 지원서 개수 조회
-	 *
-	 * @param generation 기수
-	 * @param passStatus 합격 상태
-	 * @return 지원서 개수
-	 */
-	long countByGenerationAndPassStatus(Generation generation, PassStatus passStatus);
 }
