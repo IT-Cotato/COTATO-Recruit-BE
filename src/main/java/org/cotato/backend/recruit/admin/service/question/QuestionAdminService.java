8c85088f42f8bbfb9bc8d820bbdddd3a7a4b1048
limjunseo <nonsultop1@g.hongik.ac.kr>
2026-01-12 07:10:44 +0900

    fix : questionController, service 쿼리와 수정 분리 및 네이밍 변경

--- src/main/java/org/cotato/backend/recruit/admin/service/question/QuestionAdminService.java
+++ src/main/java/org/cotato/backend/recruit/admin/service/question/QuestionAdminService.java
@@ -2,6 +2,8 @@ package org.cotato.backend.recruit.admin.service.question;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.cotato.backend.recruit.admin.dto.response.applicationQuestion.ApplicationQuestionResponse;
+import org.cotato.backend.recruit.admin.service.generation.GenerationAdminService;
 import org.cotato.backend.recruit.domain.generation.entity.Generation;
 import org.cotato.backend.recruit.domain.question.entity.Question;
 import org.cotato.backend.recruit.domain.question.enums.QuestionType;
@@ -14,16 +16,27 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class QuestionAdminService {
 	private final QuestionRepository questionRepository;
+	private final GenerationAdminService generationAdminService;
 
 	public List<Question> getQuestionsByGenerationAndQuestionType(
 			Generation generation, QuestionType questionType) {
 		return questionRepository.findByGenerationAndQuestionType(generation, questionType);
 	}
 
-	public List<Question> getQuestionsByGenerationAndQuestionTypeOrderBySequenceAsc(
+	private List<Question> getQuestionsByGenerationAndQuestionTypeOrderBySequenceAsc(
 			Generation generation, QuestionType questionType) {
 		return questionRepository.findByGenerationAndQuestionTypeOrderBySequenceAsc(
 				generation, questionType);
+	}
+
+	public List<ApplicationQuestionResponse> getApplicationQuestions(
+			Long generationId, QuestionType questionType) {
+		Generation generation = generationAdminService.getGenerationById(generationId);
+
+		List<Question> questions =
+				getQuestionsByGenerationAndQuestionTypeOrderBySequenceAsc(generation, questionType);
+
+		return questions.stream().map(ApplicationQuestionResponse::from).toList();
 	}
 
 	@Transactional
@@ -34,5 +47,10 @@ public class QuestionAdminService {
 	@Transactional
 	public void saveAll(List<Question> questions) {
 		questionRepository.saveAll(questions);
+	}
+
+	@Transactional
+	public void save(Question question) {
+		questionRepository.save(question);
 	}
 }
