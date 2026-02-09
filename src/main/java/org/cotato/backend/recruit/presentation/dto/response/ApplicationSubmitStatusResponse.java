6c97c3951ab2f961e494ceffe1e8adee9928dd70
limjunseo <nonsultop1@g.hongik.ac.kr>
2026-02-09 22:06:19 +0900

    feat : isStart 추가

--- src/main/java/org/cotato/backend/recruit/presentation/dto/response/ApplicationSubmitStatusResponse.java
+++ src/main/java/org/cotato/backend/recruit/presentation/dto/response/ApplicationSubmitStatusResponse.java
@@ -3,14 +3,15 @@ package org.cotato.backend.recruit.presentation.dto.response;
 import org.cotato.backend.recruit.domain.application.entity.Application;
 
 public record ApplicationSubmitStatusResponse(
-		Long applicationId, Boolean isSubmitted, Boolean isEnd) {
+		Long applicationId, Boolean isSubmitted, Boolean isStart, Boolean isEnd) {
 
-	public static ApplicationSubmitStatusResponse from(Application application, Boolean isEnd) {
+	public static ApplicationSubmitStatusResponse from(
+			Application application, Boolean isStart, Boolean isEnd) {
 		return new ApplicationSubmitStatusResponse(
-				application.getId(), application.getIsSubmitted(), isEnd);
+				application.getId(), application.getIsSubmitted(), isStart, isEnd);
 	}
 
-	public static ApplicationSubmitStatusResponse noApplication(Boolean isEnd) {
-		return new ApplicationSubmitStatusResponse(null, false, isEnd);
+	public static ApplicationSubmitStatusResponse noApplication(Boolean isStart, Boolean isEnd) {
+		return new ApplicationSubmitStatusResponse(null, false, isStart, isEnd);
 	}
 }
