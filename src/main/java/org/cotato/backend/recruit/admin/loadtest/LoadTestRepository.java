1dfe6be362da2c2cb3f65f0f4f551c56dec88bbd
limjunseo <nonsultop1@g.hongik.ac.kr>
2026-02-13 10:24:20 +0900

    feat : 부하테스트 종료 후 삭제

--- src/main/java/org/cotato/backend/recruit/admin/loadtest/LoadTestRepository.java
+++
@@ -1,9 +0,0 @@
-package org.cotato.backend.recruit.admin.loadtest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * 부하 테스트용 Repository
- */
-public interface LoadTestRepository extends JpaRepository<LoadTest, Long> {
-}
