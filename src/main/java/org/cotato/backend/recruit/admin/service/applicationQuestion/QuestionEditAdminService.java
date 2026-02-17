621634335912e10ac2bfb006a5acffb35bffbeab
limjunseo <nonsultop1@g.hongik.ac.kr>
2026-02-18 07:32:18 +0900

    feat : 지원서 포트폴리오 질문 수정

--- src/main/java/org/cotato/backend/recruit/admin/service/applicationQuestion/QuestionEditAdminService.java
+++ src/main/java/org/cotato/backend/recruit/admin/service/applicationQuestion/QuestionEditAdminService.java
@@ -55,14 +55,18 @@ public class QuestionEditAdminService {
 			Integer sequence, QuestionType questionType, Generation generation) {
 		String content = switch (questionType) {
 			case PM ->
-				"(선택) 추가로 제출할 포트폴리오(깃허브,블로그,노션,비핸스 등) 링크를 첨부해주세요. 업로드하실 포트폴리오 양식은 꼭"
-						+ " PDF로 변경 후 제출해주세요! (최대 크기 50MB)";
+				"(선택) 추가로 제출할 포트폴리오(깃허브,블로그,노션,비핸스 등) 링크를 첨부해주세요.\n"
+						+ "업로드하실 포트폴리오 양식은 꼭 PDF로 변경 후 제출해주세요! (최대 크기 50MB)\n"
+						+ "용량 초과시 itcotato@gmail.com으로 보내주세요.";
 			case DE ->
-				"(필수) 포트폴리오(비핸스, 노션 등) 링크를 첨부해주세요. 업로드하실 포트폴리오 양식은 꼭 PDF로 변경 후 제출해주세요! (최대 크기 50MB)"
+				"(필수) 포트폴리오(비핸스, 노션 등) 링크를 첨부해주세요.\n"
+						+ "업로드하실 포트폴리오 양식은 꼭 PDF로 변경 후 제출해주세요! (최대 크기 50MB)\n"
 						+ "용량 초과시 itcotato@gmail.com으로 보내주세요.";
 			default ->
-				"(필수) 깃허브 링크를 첨부해주세요.\n(선택) 추가로 제출할 포트폴리오(블로그,노션,비핸스 등) 링크를 첨부해주세요."
-						+ " 업로드하실 포트폴리오 양식은 꼭 PDF로 변경 후 제출해주세요! (최대 크기 50MB)";
+				"(필수) 깃허브 링크를 첨부해주세요.\n"
+						+ "(선택) 추가로 제출할 포트폴리오(블로그,노션,비핸스 등) 링크를 첨부해주세요.\n"
+						+ "업로드하실 포트폴리오 양식은 꼭 PDF로 변경 후 제출해주세요! (최대 크기 50MB)\n"
+						+ "용량 초과시 itcotato@gmail.com으로 보내주세요.";
 		};
 
 		Question question = Question.builder()
